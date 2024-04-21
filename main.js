87a60ccd8c5065f2a15b85a3339f12dc079c34c1
Edgar Agekyan <edgaragekyan@Edgars-MBP.attlocal.net>
2024-04-21 11:42:48 -0700

    figured out how to get 1 cube rotating

--- main.js
+++ main.js
@@ -1,31 +1,56 @@
 import * as THREE from 'three';
 
+let renderer, cube, scene, camera;
+
 function main() {
     const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+    renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
     const fov = 75;
     const aspect = 2;  // the canvas default
     const near = 0.1;
     const far = 5;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     
     camera.position.z = 2;
 
-    const scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
     const boxWidth = 1;
     const boxHeight = 1;
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-    const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+    const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // greenish blue
 
-    const cube = new THREE.Mesh(geometry, material);
+    cube = new THREE.Mesh(geometry, material);
 
     scene.add(cube);
 
     renderer.render(scene, camera);
   }
 
-  main();
+
+function render(time) {
+  time *= 0.001;  // convert time to seconds
+ 
+  cube.rotation.x = time;
+  cube.rotation.y = time;
+ 
+  renderer.render(scene, camera);
+ 
+  requestAnimationFrame(render);
+}
+
+
+
+main();
+
+requestAnimationFrame(render);
+
+const color = 0xFFFFFF;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
